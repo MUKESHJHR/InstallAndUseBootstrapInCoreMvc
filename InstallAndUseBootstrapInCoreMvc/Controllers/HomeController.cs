63f919a8ef6d866c847e79ccbc1bac31245c1335
mukesh kumar <mukesh.1006jhr@outlook.com>
2023-10-19 13:06:41 +0530

    Add project files.

---
+++ InstallAndUseBootstrapInCoreMvc/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using InstallAndUseBootstrapInCoreMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace InstallAndUseBootstrapInCoreMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public ViewResult Index()
+        {
+            //Create a List of Students
+            //In Real-Time, you will get the data from the database
+            List<Student> listStudents = new List<Student>()
+            {
+               new Student() { StudentId = 101, Name = "James", Branch = "CSE", Section = "A", Gender = "Male" },
+               new Student() { StudentId = 102, Name = "Smith", Branch = "ETC", Section = "B", Gender = "Male" },
+               new Student() { StudentId = 103, Name = "David", Branch = "CSE", Section = "A", Gender = "Male" },
+               new Student() { StudentId = 104, Name = "Sara", Branch = "CSE", Section = "A", Gender = "Female" },
+               new Student() { StudentId = 105, Name = "Pam", Branch = "ETC", Section = "B", Gender = "Female" }
+            };
+            //Pass the Student List to the View to make the view as a Strongly Typed View
+            return View(listStudents);
+        }
+
+        public ViewResult Details(int Id)
+        {
+            //Here, we have hard coded the student details
+            //In Real-Time, you will get the student information from the database
+            Student studentDetails = new Student() { StudentId = Id, Name = "James", Branch = "CSE", Section = "A", Gender = "Male" };
+            //Pass the Student model to the View to make the view as a Strongly Typed View
+            return View(studentDetails);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
